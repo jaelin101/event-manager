cd57a465869c120ed5259301c57b43f69bc8c9e8
Jaelin Haynes <jmhaynes@Jaelins-MacBook-Pro.local>
2020-06-14 13:27:47 -0400

    Add new event form and routing

--- event_manager/src/components/NewEvent.js
+++ event_manager/src/components/NewEvent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, Button, FormControl, Col } from 'react-bootstrap';
 
 class NewEvent extends React.Component {
 
@@ -7,7 +8,36 @@ class NewEvent extends React.Component {
     return (
       <div>
         <p>New event tab</p>
-        
+        <Form>
+          <Form.Group controlId="formEventTitle">
+            <Form.Label>Event Title</Form.Label>
+            <Form.Control placeholder="What is your event called?" />
+          </Form.Group>
+
+          <Form.Group controlId="formHostName">
+            <Form.Label>Host Name</Form.Label>
+            <Form.Control placeholder="Who is hosting the event?" />
+          </Form.Group>
+
+          <Form.Group controlId="formEventDate">
+            <Form.Label>Event Date</Form.Label>
+            <Form.Control type="date" placeholder="MM/DD/YY" />
+          </Form.Group>
+
+          <Form.Group controlId="formRegistrationLink">
+            <Form.Label>Registration Link</Form.Label>
+            <Form.Control placeholder="Where can attendees register?" />
+          </Form.Group>
+
+          <Form.Group controlId="formEventDescription">
+            <Form.Label>Event Description</Form.Label>
+            <Form.Control as="textarea" rows="3" placeholder="Describe your event" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+            Submit
+          </Button>
+        </Form>
       </div>
     );
   }
