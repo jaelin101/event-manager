158fe2a60255e11ed38d17e4814211527ce06844
Jaelin Haynes <jmhaynes@Jaelins-MacBook-Pro.local>
2020-07-26 15:55:23 -0400

    add initial function code

--- event_manager/functions/index.js
+++ event_manager/functions/index.js
@@ -1,4 +1,10 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
