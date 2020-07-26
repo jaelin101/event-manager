97b468a1e0f2e20844634f81bba0e011755b1be3
Jaelin Haynes <jmhaynes@Jaelins-MacBook-Pro.local>
2020-07-26 19:42:47 -0400

    attempted more function code

--- event_manager/functions/index.js
+++ event_manager/functions/index.js
@@ -1,23 +1,34 @@
+// require("firebase/firestore");
+// import * as admin from 'firebase-admin';
+
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
-admin.initializeApp();
-
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://event-manager-8e18d.firebaseio.com'
+  });
 const express = require('express');
 
 const api = express();
+const db = admin.firestore();
+
 
 api.get('/', (req, res) => {
 
-    const testJSON = {
-        id: 1,
-        title: 'Test Title',
-        date: '08-25-20',
-    }
+    // const eventsRef = db.collection('events');
+    // const events = eventsRef.get();
+
+    // const eventDocs = events.docs.map(doc => doc.data());
+
+    let events = (async function() {
+        const snapshot = await db.collection('events').get();
+        return snapshot.docs.map(doc => doc.data());
+    });
     
-    res.send(testJSON);
+    res.json(events);
 
 });
 
