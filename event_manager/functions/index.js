053ddbb13ecba8406553db73c420d2fbc80ce62d
Marcellus Haynes <marcellus.haynes@gmail.com>
2020-07-26 17:27:40 -0400

    Add function code

--- event_manager/functions/index.js
+++ event_manager/functions/index.js
@@ -5,10 +5,22 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const api = express();
+
+api.get('/', (req, res) => {
+
+    const testJSON = {
+        id: 1,
+        title: 'Test Title',
+        date: '08-25-20',
+    }
+    
+    res.send(testJSON);
+
+});
+
+
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(api);
